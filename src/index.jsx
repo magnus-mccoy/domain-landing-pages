aa4b1048e4bb3a57c21af0d39997e0b341019613
fmfg03 <105071395+fmfg03@users.noreply.github.com>
2025-02-06 16:50:48 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { DomainLanding } from './components/DomainLanding.jsx';
-
+import DomainLanding from './components/DomainLanding.jsx';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(React.createElement(DomainLanding));
