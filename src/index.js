c1f28a22e14af373d384ca64c968d2d9c802abd9
fmfg03 <105071395+fmfg03@users.noreply.github.com>
2025-02-06 16:18:46 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import DomainLanding from './components/DomainLanding';
+import { DomainLanding } from './components/DomainLanding';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<DomainLanding />);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(React.createElement(DomainLanding));
