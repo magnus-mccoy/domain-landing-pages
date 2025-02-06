c1999fd1b4fe90a912a84366c4634d650edb105c
fmfg03 <105071395+fmfg03@users.noreply.github.com>
2025-02-06 11:55:12 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import DomainLanding from './components/DomainLanding';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<DomainLanding />);
